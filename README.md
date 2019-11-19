e965c32c6280e03ef5b5dab2fcce81025c9eaabf
singhwong <singhwongwxg@hotmail.com>
2019-11-19 22:53:07 +0800

    commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Screen Recorder
+
+A simple screen recorder using both the Windows.Graphics.Capture and Windows.Media.Transcoding APIs.
+
+![A screenshot of SimpleRecorder.](https://user-images.githubusercontent.com/7089228/57436318-4b40af00-71f4-11e9-9ffd-708bed643ddd.png)
---
+++ SimpleRecorder/MainPage.xaml.cs
@@ -0,0 +1,351 @@
+﻿using CaptureEncoder;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Composition;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Media;
+
+namespace SimpleRecorder
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Setup();
+            if (!GraphicsCaptureSession.IsSupported())
+            {
+                IsEnabled = false;
+
+                var dialog = new MessageDialog(
+                    "Screen capture is not supported on this device for this release of Windows!",
+                    "Screen capture unsupported");
+
+                var ignored = dialog.ShowAsync();
+                return;
+            }
+
+            _device = Direct3D11Helpers.CreateDevice();
+
+            var settings = GetCachedSettings();
+
+            var names = new List<string>();
+            names.Add(nameof(VideoEncodingQuality.HD1080p));
+            names.Add(nameof(VideoEncodingQuality.HD720p));
+            names.Add(nameof(VideoEncodingQuality.Uhd2160p));
+            names.Add(nameof(VideoEncodingQuality.Uhd4320p));
+            QualityComboBox.ItemsSource = names;
+            QualityComboBox.SelectedIndex = names.IndexOf(settings.Quality.ToString());
+
+            var frameRates = new List<string> { "30fps", "60fps" };
+            FrameRateComboBox.ItemsSource = frameRates;
+            FrameRateComboBox.SelectedIndex = frameRates.IndexOf($"{settings.FrameRate}fps");
+
+            UseCaptureItemSizeCheckBox.IsChecked = settings.UseSourceSize;
+        }
+
+        private async void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var button = (ToggleButton)sender;
+
+            // Get our encoder properties
+            var frameRate = uint.Parse(((string)FrameRateComboBox.SelectedItem).Replace("fps", ""));
+            var quality = (VideoEncodingQuality)Enum.Parse(typeof(VideoEncodingQuality), (string)QualityComboBox.SelectedItem, false);
+            var useSourceSize = UseCaptureItemSizeCheckBox.IsChecked.Value;
+
+            var temp = MediaEncodingProfile.CreateMp4(quality);
+            var bitrate = temp.Video.Bitrate;
+            var width = temp.Video.Width;
+            var height = temp.Video.Height;
+
+            // Get our capture item
+            var picker = new GraphicsCapturePicker();
+            var item = await picker.PickSingleItemAsync();
+            if (item == null)
+            {
+                button.IsChecked = false;
+                return;
+            }
+            StartCaptureInternal(item);
+            // Use the capture item's size for the encoding if desired
+            if (useSourceSize)
+            {
+                width = (uint)item.Size.Width;
+                height = (uint)item.Size.Height;
+
+                // Even if we're using the capture item's real size,
+                // we still want to make sure the numbers are even.
+                // Some encoders get mad if you give them odd numbers.
+                width = EnsureEven(width);
+                height = EnsureEven(height);
+            }
+
+            // Find a place to put our vidoe for now
+            var file = await GetTempFileAsync();
+
+            // Tell the user we've started recording
+            MainTextBlock.Text = "● rec";
+            var originalBrush = MainTextBlock.Foreground;
+            MainTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+            MainProgressBar.IsIndeterminate = true;
+
+            // Kick off the encoding
+            try
+            {
+                using (var stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                using (_encoder = new Encoder(_device, item))
+                {
+                    await _encoder.EncodeAsync(
+                        stream, 
+                        width, height, bitrate, 
+                        frameRate);
+                }
+                MainTextBlock.Foreground = originalBrush;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(ex);
+
+                var dialog = new MessageDialog(
+                    $"Uh-oh! Something went wrong!\n0x{ex.HResult:X8} - {ex.Message}",
+                    "Recording failed");
+
+                await dialog.ShowAsync();
+
+                button.IsChecked = false;
+                MainTextBlock.Text = "failure";
+                MainTextBlock.Foreground = originalBrush;
+                MainProgressBar.IsIndeterminate = false;
+                return;
+            }
+
+            // At this point the encoding has finished,
+            // tell the user we're now saving
+            MainTextBlock.Text = "saving...";
+
+            // Ask the user where they'd like the video to live
+            var newFile = await PickVideoAsync();
+            if (newFile == null)
+            {
+                // User decided they didn't want it
+                // Throw out the encoded video
+                button.IsChecked = false;
+                MainTextBlock.Text = "canceled";
+                MainProgressBar.IsIndeterminate = false;
+                await file.DeleteAsync();
+                return;
+            }
+            // Move our vidoe to its new home
+            await file.MoveAndReplaceAsync(newFile);
+
+            // Tell the user we're done
+            button.IsChecked = false;
+            MainTextBlock.Text = "done";
+            MainProgressBar.IsIndeterminate = false;
+
+            // Open the final product
+            await Launcher.LaunchFileAsync(newFile);
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // If the encoder is doing stuff, tell it to stop
+            //captureSession.Dispose();
+            _encoder?.Dispose();
+        }
+
+        private async Task<StorageFile> PickVideoAsync()
+        {
+            var picker = new FileSavePicker();
+            picker.SuggestedStartLocation = PickerLocationId.VideosLibrary;
+            picker.SuggestedFileName = "recordedVideo";
+            picker.DefaultFileExtension = ".mp4";
+            picker.FileTypeChoices.Add("MP4 Video", new List<string> { ".mp4" });
+
+            var file = await picker.PickSaveFileAsync();
+            return file;
+        }
+
+        private async Task<StorageFile> GetTempFileAsync()
+        {
+            var folder = ApplicationData.Current.TemporaryFolder;
+            var name = DateTime.Now.ToString("yyyyMMdd-HHmm-ss");
+            var file = await folder.CreateFileAsync($"{name}.mp4");
+            return file;
+        }
+
+        private uint EnsureEven(uint number)
+        {
+            if (number % 2 == 0)
+            {
+                return number;
+            }
+            else
+            {
+                return number + 1;
+            }
+        }
+
+        private AppSettings GetCurrentSettings()
+        {
+            var quality = ParseEnumValue<VideoEncodingQuality>((string)QualityComboBox.SelectedItem);
+            var frameRate = uint.Parse(((string)FrameRateComboBox.SelectedItem).Replace("fps", ""));
+            var useSourceSize = UseCaptureItemSizeCheckBox.IsChecked.Value;
+
+            return new AppSettings { Quality = quality, FrameRate = frameRate, UseSourceSize = useSourceSize };
+        }
+
+        private AppSettings GetCachedSettings()
+        {
+            var localSettings = ApplicationData.Current.LocalSettings;
+            var result =  new AppSettings
+            {
+                Quality = VideoEncodingQuality.HD1080p,
+                FrameRate = 60,
+                UseSourceSize = true
+            };
+            if (localSettings.Values.TryGetValue(nameof(AppSettings.Quality), out var quality))
+            {
+                result.Quality = ParseEnumValue<VideoEncodingQuality>((string)quality);
+            }
+            if (localSettings.Values.TryGetValue(nameof(AppSettings.FrameRate), out var frameRate))
+            {
+                result.FrameRate = (uint)frameRate;
+            }
+            if (localSettings.Values.TryGetValue(nameof(AppSettings.UseSourceSize), out var useSourceSize))
+            {
+                result.UseSourceSize = (bool)useSourceSize;
+            }
+            return result;
+        }
+
+        public void CacheCurrentSettings()
+        {
+            var settings = GetCurrentSettings();
+            CacheSettings(settings);
+        }
+
+        private static void CacheSettings(AppSettings settings)
+        {
+            var localSettings = ApplicationData.Current.LocalSettings;
+            localSettings.Values[nameof(AppSettings.Quality)] = settings.Quality.ToString();
+            localSettings.Values[nameof(AppSettings.FrameRate)] = settings.FrameRate;
+            localSettings.Values[nameof(AppSettings.UseSourceSize)] = settings.UseSourceSize;
+        }
+
+        private static T ParseEnumValue<T>(string input)
+        {
+            return (T)Enum.Parse(typeof(T), input, false);
+        }
+
+        struct AppSettings
+        {
+            public VideoEncodingQuality Quality;
+            public uint FrameRate;
+            public bool UseSourceSize;
+        }
+
+        private IDirect3DDevice _device;
+        private Encoder _encoder;
+
+        private void Setup()
+        {
+            _canvasDevice = new CanvasDevice();
+
+            var compositionGraphicsDevice = CanvasComposition.CreateCompositionGraphicsDevice(
+                Window.Current.Compositor,
+                _canvasDevice);
+            //Compositor com = new Compositor();
+            var compositor = Window.Current.Compositor;
+
+            _surface = compositionGraphicsDevice.CreateDrawingSurface(
+                new Size(1000, 600),
+                DirectXPixelFormat.B8G8R8A8UIntNormalized,
+                DirectXAlphaMode.Premultiplied);
+            // 现在只有这个参数能在 Composition 使用
+
+            var visual = compositor.CreateSpriteVisual();
+            visual.RelativeSizeAdjustment = Vector2.One;
+            var brush = compositor.CreateSurfaceBrush(_surface);
+            brush.Stretch = CompositionStretch.Uniform;
+            visual.Brush = brush;
+            ElementCompositionPreview.SetElementChildVisual(mainGrid, visual);
+
+            _compositionGraphicsDevice = compositionGraphicsDevice;
+        }
+
+        private CanvasDevice _canvasDevice;
+        private CompositionDrawingSurface _surface;
+
+        // 下面属性防止内存回收
+        private CompositionGraphicsDevice _compositionGraphicsDevice;
+        private Direct3D11CaptureFramePool _direct3D11CaptureFramePool;
+        private GraphicsCaptureSession _graphicsCaptureSession;
+
+        private void StartCaptureInternal(GraphicsCaptureItem item)
+        {
+            // 下面参数暂时不能修改
+            Direct3D11CaptureFramePool framePool = Direct3D11CaptureFramePool.Create(
+                _canvasDevice, // D3D device
+                DirectXPixelFormat.B8G8R8A8UIntNormalized, // Pixel format
+                                                           // 要在其中存储捕获的框架的缓冲区数量
+                1,
+                // 每个缓冲区大小
+                item.Size); // Size of the buffers
+
+            framePool.FrameArrived += (s, a) =>
+            {
+                using (var frame = framePool.TryGetNextFrame())
+                {
+                    try
+                    {
+                        // 将获取到的 Direct3D11CaptureFrame 转 win2d 的
+                        CanvasBitmap canvasBitmap = CanvasBitmap.CreateFromDirect3D11Surface(
+                            _canvasDevice,
+                            frame.Surface);
+
+                        CanvasComposition.Resize(_surface, canvasBitmap.Size);
+
+                        using (var session = CanvasComposition.CreateDrawingSession(_surface))
+                        {
+                            session.Clear(Colors.Transparent);
+                            session.DrawImage(canvasBitmap);
+                        }
+                    }
+                    catch (Exception e) when (_canvasDevice.IsDeviceLost(e.HResult))
+                    {
+                        // 设备丢失
+                    }
+                }
+            };
+
+            captureSession = framePool.CreateCaptureSession(item);
+            captureSession.StartCapture();
+
+            // 作为字段防止内存回收
+            _direct3D11CaptureFramePool = framePool;
+            _graphicsCaptureSession = captureSession;
+        }
+
+        private GraphicsCaptureSession captureSession;
+    }        
+}
